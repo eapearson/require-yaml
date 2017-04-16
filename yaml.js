6b93632df815c824cfe7b91b80f7392150df7f5f
Erik Pearson <eapearson@lbl.gov>
2017-04-16 09:14:05 -0700

    switch to yaml2 - testing is messed up, probably a conflict in browserfied yaml2 and the test config

--- yaml.js
+++ yaml.js
@@ -1,27 +1,39 @@
-define(['text', 'js-yaml'], function(text, jsYaml){
-	var
-	buildMap = {};
+define([
+    'text',
+    'yaml2'
+], function(
+    text,
+    yaml
+) {
+    'use strict';
+    var buildMap = {};
 
-	return {
-		write: function(pluginName, name, write){
-			if (name in buildMap){
-				write('define("' + pluginName + '!' + name + '", function(){ return ' + buildMap[name] + '; });\n');
-			}
-		}
-		, load: function (name, parentRequire, onload, config){
-			text.get(parentRequire.toUrl(name), function(yamlString){
-				try{
-					var result = jsYaml.safeLoad(yamlString);
-					if (config.isBuild){
-						buildMap[name] = JSON.stringify(result);
-					}
-					onload(result);
-				} catch (e){
-					onload.error(e);
-				}
-			});
+    return {
+        write: function(pluginName, name, write) {
+            if (name in buildMap) {
+                write('define("' +
+                    pluginName +
+                    '!' +
+                    name +
+                    '", function(){ return ' +
+                    buildMap[name] +
+                    '; });\n');
+            }
+        },
+        load: function(name, parentRequire, onload, config) {
+            text.get(parentRequire.toUrl(name), function(yamlString) {
+                try {
+                    var result = yaml.parse(yamlString);
+                    if (config.isBuild) {
+                        buildMap[name] = JSON.stringify(result);
+                    }
+                    onload(result);
+                } catch (e) {
+                    onload.error(e);
+                }
+            });
 
-		}
-		, version : '0.1.1'
-	};
+        },
+        version: '1.0.0'
+    };
 });
